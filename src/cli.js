f15f73389d972df73afd8d245930fa50200cd428
Georgiy Melikhov <g.melihov@electroneek.com>
2021-01-10 17:36:00 +0300

    add linter

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
-function welcomeUser() {
-    console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${name}!`);
-};
-
-export { welcomeUser };
+export default function welcomeUser() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+}
